9100523b164a1d9f60e8657d97d39a0b4744e8a1
Andrea Barrera <andrea.barrera7@gmail.com>
2023-02-11 21:56:21 -0500

    Finishing app.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className="main-container">
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimony />
+        <Testimony
+          name="Shawn Wang"
+          country="Singapore"
+          image="1"
+          charge="Software Engineer"
+          enterprise="Amazon"
+          testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+        <Testimony
+          name="Sarah Chima"
+          country="Nigeria"
+          image="2"
+          charge="Software Engineer"
+          enterprise="ChatDesk"
+          testimony="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        />
+        <Testimony
+          name="Emma Bostian"
+          country="Sweden"
+          image="3"
+          charge="Software Engineer"
+          enterprise="Spotify"
+          testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
       </div>
     </div>
   );
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,23 +1,22 @@
 import React from "react";
+import "../stylesheets/Testimony.css";
 
-function Testimony() {
+function Testimony(props) {
   return (
     <div className="testimony-container">
       <img
         className="testimony-image"
-        src={require("../images/im3.png")}
+        src={require(`../images/im${props.image}.png`)}
         alt="Emma's picture"
       />
-      <div className="container-testimony-text">
-        <p className="testimony-name">Emma Bostian in Sweden</p>
-        <p className="testimony-charge">Software Engineer at Spotify</p>
-        <p className="testimony-text">
-          "I've always struggled with learning JavaScript. I've taken many
-          courses but freeCodeCamp's course was the one which stuck. Studying
-          JavaScript as well as data structures and algorithms on freeCodeCamp
-          gave me the skills and confidence I needed to land my dream job as a
-          software engineer at Spotify."
+      <div className="testimony-text-container">
+        <p className="testimony-name">
+          <strong>{props.name}</strong> in {props.country}
         </p>
+        <p className="testimony-charge">
+          {props.charge} at <strong>{props.enterprise}</strong>
+        </p>
+        <p className="testimony-text">"{props.testimony}"</p>
       </div>
     </div>
   );
