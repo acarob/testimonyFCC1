fbdbf3dcdbcbcf6e8ccbf0b24bd51df2db96bb7c
Andrea Barrera <andrea.barrera7@gmail.com>
2023-08-28 22:18:26 -0500

    Changing components from functional to class

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
+import React from "react";
 import "./App.css";
 import Testimony from "./components/Testimony";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="main-container">
-        <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimony
-          name="Shawn Wang"
-          country="Singapore"
-          image="1"
-          charge="Software Engineer"
-          enterprise="Amazon"
-          testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer."
-          quote="freeCodeCamp changed my life."
-        />
-        <Testimony
-          name="Sarah Chima"
-          country="Nigeria"
-          image="2"
-          charge="Software Engineer"
-          enterprise="ChatDesk"
-          testimony2=" as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
-          quote="freeCodeCamp was the gateway to my career"
-        />
-        <Testimony
-          name="Emma Bostian"
-          country="Sweden"
-          image="3"
-          charge="Software Engineer"
-          enterprise="Spotify"
-          testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on "
-          quote="freeCodeCamp gave me the skills"
-          testimony2="and confidence I needed to land my dream job as a software engineer at Spotify."
-        />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="main-container">
+          <h1>Here is what our alumni say about freeCodeCamp:</h1>
+          <Testimony
+            name="Shawn Wang"
+            country="Singapore"
+            image="1"
+            charge="Software Engineer"
+            enterprise="Amazon"
+            testimony="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer."
+            quote="freeCodeCamp changed my life."
+          />
+          <Testimony
+            name="Sarah Chima"
+            country="Nigeria"
+            image="2"
+            charge="Software Engineer"
+            enterprise="ChatDesk"
+            testimony2=" as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+            quote="freeCodeCamp was the gateway to my career"
+          />
+          <Testimony
+            name="Emma Bostian"
+            country="Sweden"
+            image="3"
+            charge="Software Engineer"
+            enterprise="Spotify"
+            testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on "
+            quote="freeCodeCamp gave me the skills"
+            testimony2="and confidence I needed to land my dream job as a software engineer at Spotify."
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,28 +1,30 @@
 import React from "react";
 import "../stylesheets/Testimony.css";
 
-function Testimony(props) {
-  return (
-    <div className="testimony-container">
-      <img
-        className="testimony-image"
-        src={require(`../images/im${props.image}.png`)}
-        alt="not found"
-      />
-      <div className="testimony-text-container">
-        <p className="testimony-name">
-          <strong>{props.name}</strong> in {props.country}
-        </p>
-        <p className="testimony-charge">
-          {props.charge} at <strong>{props.enterprise}</strong>
-        </p>
-        <p className="testimony-text">
-          "{props.testimony} <strong>{props.quote} </strong>
-          {props.testimony2}"
-        </p>
+class Testimony extends React.Component {
+  render() {
+    return (
+      <div className="testimony-container">
+        <img
+          className="testimony-image"
+          src={require(`../images/im${this.props.image}.png`)}
+          alt={`${this.props.name}`}
+        />
+        <div className="testimony-text-container">
+          <p className="testimony-name">
+            <strong>{this.props.name}</strong> in {this.props.country}
+          </p>
+          <p className="testimony-charge">
+            {this.props.charge} at <strong>{this.props.enterprise}</strong>
+          </p>
+          <p className="testimony-text">
+            "{this.props.testimony} <strong>{this.props.quote} </strong>
+            {this.props.testimony2}"
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Testimony;
